1ca5e0b710a725cc3fafe27aae728db20c146a7c
Diego Abelenda <diego.abelenda@camptocamp.com>
2016-02-26 12:37:21 +0100

    Add basic acceptance tests

---
+++ spec/acceptance/rsyslogv8_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper_acceptance'
+
+describe 'rsyslogv8' do
+
+  context 'with defaults' do
+    it 'should apply without error' do
+      pp = <<-EOS
+        class { 'rsyslogv8': }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'rsyslogv8': }
+      EOS
+
+      apply_manifest(pp, :catch_changes => true)
+    end
+    it 'should idempotently run bis' do
+      pp = <<-EOS
+        class { 'rsyslogv8': }
+      EOS
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+end
+
