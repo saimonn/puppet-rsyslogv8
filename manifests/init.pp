2578932314dff27629c0b55a69427e30bd630725
Diego Abelenda <diego.abelenda@camptocamp.com>
2016-02-26 13:39:35 +0100

    Include classes using absolute name instead of relative

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class rsyslogv8 (
 ) inherits rsyslogv8::params {
-  include rsyslogv8::install
-  include rsyslogv8::config
-  include rsyslogv8::service
+  include ::rsyslogv8::install
+  include ::rsyslogv8::config
+  include ::rsyslogv8::service
 }
 
