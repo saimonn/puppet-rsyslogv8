e8d15ee9c7c2fe13e54150297cd0e575489eac58
Diego Abelenda <diego.abelenda@camptocamp.com>
2016-03-14 09:23:39 +0100

    Add strict tests for configuration and remote logging

--- spec/acceptance/rsyslogv8_spec.rb
+++ spec/acceptance/rsyslogv8_spec.rb
@@ -19,6 +19,41 @@ describe 'rsyslogv8' do
 
       apply_manifest(pp, :catch_changes => true)
     end
+    describe command('rsyslogd -N1') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
+
+  context 'with log shipping TCP without TLS' do
+    it 'should apply without error' do
+      pp = <<-EOS
+        class { 'rsyslogv8': }
+        rsyslogv8::config::ship { 'localhost':
+          remote_port      => 11111,
+          remote_auth      => 'anon',
+          queue_size_limit => 100000,
+          queue_mode       => 'LinkedList-DA',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'rsyslogv8': }
+        rsyslogv8::config::ship { 'localhost':
+          remote_port      => 11111,
+          remote_auth      => 'anon',
+          queue_size_limit => 100000,
+          queue_mode       => 'LinkedList-DA',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_changes => true)
+    end
+    describe command('rsyslogd -N1') do
+      its(:exit_status) { should eq 0 }
+    end
   end
 end
 
