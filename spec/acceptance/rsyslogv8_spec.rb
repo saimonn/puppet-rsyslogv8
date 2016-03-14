d0da6d80e66435b1b4b60d1e569e95936444a52a
Diego Abelenda <diego.abelenda@camptocamp.com>
2016-03-14 14:55:38 +0100

    Add acceptance tests to validate the receiver works

--- spec/acceptance/rsyslogv8_spec.rb
+++ spec/acceptance/rsyslogv8_spec.rb
@@ -55,5 +55,44 @@ describe 'rsyslogv8' do
       its(:exit_status) { should eq 0 }
     end
   end
+
+  context 'with log receiving TCP without TLS' do
+    it 'should apply without error' do
+      pp = <<-EOS
+        class { 'rsyslogv8':
+          modules_extras => { 'imtcp' => {} }
+        }
+        rsyslogv8::config::receive { 'localhost':
+          port             => 11112,
+          protocol         => 'tcp',
+          queue_size_limit => 100000,
+          queue_mode       => 'LinkedList-DA',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
+    it 'should idempotently run' do
+      pp = <<-EOS
+        class { 'rsyslogv8':
+          modules_extras => { 'imtcp' => {} }
+        }
+        rsyslogv8::config::receive { 'localhost':
+          port             => 11112,
+          protocol         => 'tcp',
+          queue_size_limit => 100000,
+          queue_mode       => 'LinkedList-DA',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_changes => true)
+    end
+    describe command('rsyslogd -N1') do
+      its(:exit_status) { should eq 0 }
+    end
+    describe port(11112) do
+      it { should be_listening }
+    end
+  end
 end
 
